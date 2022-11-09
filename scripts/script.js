00c83306851fcc5e8cd7bdf3f2c7ab6eb594efa2
Anh <minhanhle@hotmail.com>
2022-11-08 20:54:54 -0800

    add digit equal number, courselist, and success log

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const courseList = [{code: "ACIT 1620", name: "Web Fundamental Technologies", term: "Fall 2022"}, {code: "ACIT 1420", name: "Intro to System Administrator", term:"Fall 2022"}, {code: "ACIT 1630", name: "Database Systems", term: "Fall 2022"}]
+const courseList = [{code: "ACIT 1620", name: "Web Fundamental Technologies"}, {code: "ACIT 1420", name: "Intro to System Administrator"}, {code: "ACIT 1630", name: "Database Systems"}]
 
 console.log(courseList);
 
@@ -6,6 +6,24 @@ let input = "";
 do {
     input = (prompt("Enter code:"));   
 } while (input.length !=4 || isNaN(input))
+
+
+let x = false
+for (let prop of courseList)
+    {
+        if (prop["code"].includes(input))
+        {
+            console.log(`Yes I am taking the course: ${prop.code} - ${prop.name}`)
+            x = true
+        }
+    }
+
+if (x === false)
+    {
+digit = Number
+courseList.push({"code": digit, "name": null})
+    console.log("Success")
+    }
 
 
 
