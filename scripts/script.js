89a606bbd3c4c5cfb0b2f112086fcdfe67df4d57
Anh <minhanhle@hotmail.com>
2022-11-08 19:14:59 -0800

    fix enter code prompt to not equal length of 4 + is not a number

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,11 @@
-let courseList = {
-    code: "ACIT 1620",
-    name: "Web Fundamental Technologies",
-    term: "Fall 2022",
-}
+const courseList = [{code: "ACIT 1620", name: "Web Fundamental Technologies", term: "Fall 2022"}, {code: "ACIT 1420", name: "Intro to System Administrator", term:"Fall 2022"}, {code: "ACIT 1630", name: "Database Systems", term: "Fall 2022"}]
 
 console.log(courseList);
+
+let input = "";
+do {
+    input = (prompt("Enter code:"));   
+} while (input.length !=4 || isNaN(input))
+
+
+
